4c3c893a70772859f983c37542dfd55b876185c0
Guilherme Rosa <guilherme@live.com>
2020-04-22 15:23:25 -0300

    criando a pagina de detalhes das noticias

--- app/routes/web.js
+++ app/routes/web.js
@@ -10,8 +10,17 @@ module.exports = function(app){
 
 	app.get('/noticias', function(req, res){
 		var connection = app.config.dbConnection();
+
 		connection.query('select * from noticias', function(error, result){
 			res.render('noticias/noticias', {noticias: result});
+		});
+	});
+
+	app.get('/noticia', function(req, res){
+		var connection = app.config.dbConnection();
+
+		connection.query('select * from noticias where id = 1', function(error, result){
+			res.render('noticias/noticia', {noticia: result});
 		});
 	});
 };
