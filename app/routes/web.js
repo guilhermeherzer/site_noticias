201874b44bf3ee8914a5fd90e82218d811aca6ae
Guilherme Rosa <guilherme@live.com>
2020-04-22 15:40:12 -0300

    implementando models

--- app/routes/web.js
+++ app/routes/web.js
@@ -11,7 +11,9 @@ module.exports = function(app){
 	app.get('/noticias', function(req, res){
 		var connection = app.config.dbConnection();
 
-		connection.query('select * from noticias', function(error, result){
+		var noticiasModel = app.app.models.noticiasModel;
+
+		noticiasModel.getNoticias(connection, function(error, result){
 			res.render('noticias/noticias', {noticias: result});
 		});
 	});
@@ -19,7 +21,9 @@ module.exports = function(app){
 	app.get('/noticia', function(req, res){
 		var connection = app.config.dbConnection();
 
-		connection.query('select * from noticias where id = 1', function(error, result){
+		var noticiasModel = app.app.models.noticiasModel;
+
+		noticiasModel.getNoticia(connection, function(error, result){
 			res.render('noticias/noticia', {noticia: result});
 		});
 	});
